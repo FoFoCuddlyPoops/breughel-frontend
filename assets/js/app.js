74ef1a082156076ed0aad92ab9af62a35314d0cd
Kevin Ryckaert <ryckaerkevin@gmail.com>
2016-02-25 18:58:43 +0100

    getting wow to work

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,17 +1,35 @@
 'use strict';
 require('angular');
 require('./app-bootstrap.js');
+//require('../../bower_components/wow/dist/wow.js');
+require('wow');
 
 (function() {
     var app = angular.module('breughel',
         [
             // All the parent modules
-            'breughel.module.core',
-            'breughel.module.services',
-            'breughel.module.finder',
-            'breughel.module.authentication',
-            'breughel.module.authorization',
-            'breughel.module.registration',
-            'breughel.module.administration',
-        ]);
+            //'breughel.module.core',
+            //'breughel.module.services',
+            //'breughel.module.finder',
+            //'breughel.module.authentication',
+            //'breughel.module.authorization',
+            //'breughel.module.registration',
+            //'breughel.module.administration',
+            //'breughel.module.admin',
+            //'wowjs'
+        ])
+        .controller('Controller', ['$scope', function($scope) {
+            $scope.customer = {
+                name: 'Naomi',
+                address: '1600 Amphitheatre'
+            };
+        }])
+        .directive('myCustomer', function() {
+            return {
+                template: 'Name: {{customer.name}} Address: {{customer.address}}'
+            };
+        })
+        .config(function() {
+            new WOW().init();
+        });
 })();
--- assets/js/modules/admin/profile/index.js
+++ assets/js/modules/admin/profile/index.js
@@ -1,11 +1,6 @@
 'use strict';
 
-require('angular');
-require('angular-ui-router');
 require('./modules/admin/profile/index.js');
-require('./modules/admin/education/index.js');
-require('./modules/admin/experience/index.js');
-require('./modules/admin/certificates/index.js');
 require('./modules/admin/account/index.js');
 
 module.exports = angular.module('breughel.module.administration',
@@ -13,8 +8,5 @@ module.exports = angular.module('breughel.module.administration',
         'ui.router',
         // All the administration submodules
         'breughel.module.administration.profile',
-        'breughel.module.administration.education',
-        'breughel.module.administration.experience',
-        'breughel.module.administration.certificates',
         'breughel.module.administration.account'
     ]);
